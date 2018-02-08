9d06073f65da808700a52e5a2ad9a5a7d4dfd648
omarwaleed <omarpersonalprofile@gmail.com>
2018-02-08 19:11:33 +0200

    required the needed modules in app js and created the api route and its file

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const muler = require('multer');
+global.Promise = require('bluebird');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+const index = require('./routes/index');
+const api = require('./routes/api');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,11 +27,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api', api);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+const Product = require('../models/Product');
+const Order = require('../models/Order');
+const Category = require('../models/Category');
+const Brand = require('../models/Brand');
