96c1b4f81bfa99efffe0d0dd3f379d9ac54baeaf
omarwaleed <omarpersonalprofile@gmail.com>
2018-02-09 05:27:48 +0200

    added basic crud operations for user and product models

--- routes/api.js
+++ routes/api.js
@@ -6,3 +6,60 @@ const Product = require('../models/Product');
 const Order = require('../models/Order');
 const Category = require('../models/Category');
 const Brand = require('../models/Brand');
+
+router.post('/login', (req, res)=>{
+	res.sendStatus(501);
+})
+
+router.get('/auth', (req, res, next)=>{
+	res.sendStatus(200);
+})
+
+router.get('/users', (req, res)=>{
+	User.find({}).lean()
+	.then((users)=>{
+		res.json(users);
+	})
+	.catch((err)=>{
+		console.error(err);
+		res.sendStatus(500);
+	})
+})
+
+router.get('/users/:id', (req, res)=>{
+	User.findById(req.params.id).lean()
+	.then((user)=>{
+		res.json(user);
+	})
+	.catch((err)=>{
+		console.error(err);
+		res.sendStatus(500);
+	})
+})
+
+router.get('/products', (req, res)=>{
+	Product.find({}).lean()
+	.then((products)=>{
+		res.json(products)
+	})
+	.catch((err)=>{
+		console.error(err);
+		res.sendStatus(500);
+	})
+})
+
+router.get('/products/:id', (req, res)=>{
+
+	let { sortBy, sortDirection, filter }
+
+	Product.findById(req.params.id).lean()
+	.then((product)=>{
+		res.json(product);
+	})
+	.catch((err)=>{
+		console.error(err);
+		res.sendStatus(500);
+	})
+})
+
+module.exports = router;
