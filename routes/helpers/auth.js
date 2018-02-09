eb786e8138e2c82c728e44215927e8735dc02054
omarwaleed <omarpersonalprofile@gmail.com>
2018-02-09 05:27:21 +0200

    added helper file with athenticate user middleware

---
+++ routes/helpers/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET || 'sUG6akIrGG4T5eEGTc6Caau825btqttL1uPZlCqx';
+const User = require('../../models/User');
+
+module.exports = {
+	authenticateUser: function(req, res, next){
+		jwt.verify(req.headers['X-Auth-Token'], jwtSecret, function(err, decoded){
+			if(err){
+				return res.sendStatus(401);
+			}
+			User.findById(decoded.id)
+			.then((user)=>{
+				req.user = user;
+				next();
+			})
+			.catch((err)=>{
+				console.error(err);
+				res.sendStatus(500);
+			})
+		})
+	}
+}
