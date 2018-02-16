d3e048dbd6629bb42c94713665f2bd9e41507183
omarwaleed <omarpersonalprofile@gmail.com>
2018-02-16 10:42:33 +0200

    added a helper to remove empty key objects

---
+++ routes/helpers/helpers.js
@@ -0,0 +1,11 @@
+module.exports = {
+	removeEmptyObjectKeys = (givenObject)=>{
+		let theObject = {};
+		Object.keys(theObject).map((key)=>{
+			if(givenObject[key]){
+				theObject[key] = givenObject[key];
+			}
+		})
+		return theObject;
+	}
+}
