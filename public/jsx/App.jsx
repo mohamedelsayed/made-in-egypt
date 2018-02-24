b9c49cb4c286367ac00a53242436fc0bdca63837
omarwaleed <omarpersonalprofile@gmail.com>
2018-02-24 10:18:11 +0200

    created the main structure with change view function and handling initial execution on start of the app

--- public/jsx/App.jsx
+++ public/jsx/App.jsx
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { LocaleProvider } from 'antd';
-import en_US from 'antd/lib/locale-provider/en_US';
+import { Container } from 'semantic-ui-react';
+import axios from 'axios';
+
+import Login from './Login.jsx';
 
 export default class App extends Component {
 	constructor(){
 		super();
 		this.state = {
+			currentView: <div></div>
+		}
+	}
 
+	changeView = (view)=>{
+		let componentShortCut = (ViewComponent)=>{
+			return this.setState({currentView: <ViewComponent changeView={this.changeView} />})
+		}
+		switch(view){
+			case 'login': componentShortCut(Login);
+			default: <div />
+		}
+	}
+
+	componentDidMount(){
+		let auth = localStorage.getItem('auth');
+		if(auth){
+			axios.get('localhost:3000/api/admin/auth', {
+				headers: {
+					'x-auth-token': auth
+				}
+			})
+			.then((response)=>{
+				if(response.status == 200){
+					this.changeView('dashboard');
+				} else {
+					console.warn('Something is wrong');
+					this.changeView('login');
+				}
+			})
+			.catch((err)=>{
+				console.warn(err);
+				this.changeView('login')
+			})
+		} else {
+			this.changeView('login');
 		}
 	}
 
 	render(){
 		return(
-			<LocaleProvider locale={en_US}>
-				
-			</LocaleProvider>
+			<Container>
+				{
+					this.state.currentView
+				}
+			</Container>
 		)
 	}
 }
