16fa38169f18cfb37614236fcb9dc0f852f8135b
omarwaleed <omarpersonalprofile@gmail.com>
2018-04-14 23:05:00 +0200

    added paymob cofig

--- routes/helpers/config.js
+++ routes/helpers/config.js
@@ -1,3 +1,8 @@
 module.exports = {
-	jwtSecret: process.env.JWT_SECRET || 'sUG6akIrGG4T5eEGTc6Caau825btqttL1uPZlCqx'
+	jwtSecret: process.env.JWT_SECRET || 'sUG6akIrGG4T5eEGTc6Caau825btqttL1uPZlCqx',
+	paymob: {
+		username: "MadeInEgypt" || process.env.PAYMOB_USERNAME,
+		password: "MyP@ssw0rd1" || process.env.PAYMOB_PASSWORD,
+		cardIntegrationId: "1141" || process.env.PAYMOB_CID
+	}
 }
