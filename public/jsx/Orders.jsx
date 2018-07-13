9c0954096708b1347f8fc22b34bb9a0e9d24e044
omarwaleed <omarpersonalprofile@gmail.com>
2018-07-13 13:39:18 +0200

    handled order pagination

--- public/jsx/Orders.jsx
+++ public/jsx/Orders.jsx
@@ -9,10 +9,24 @@ export default class Orders extends Component {
 		super();
 		this.state = {
 			orders: [],
+			pageNumber: 1
 		}
 	}
 	componentDidMount(){
 		axios.get(`/api/admin/orders`, {
+			headers: {
+				'x-auth-token': localStorage.getItem('auth')
+			}
+		})
+		.then((response)=>{
+			this.setState({orders: response.data});
+		})
+		.catch((err)=>{
+			console.error(err);
+		})
+	}
+	handleUpdatePage = ()=>{
+		axios.get(`/api/admin/orders?pageNumber=${this.state.pageNumber}`, {
 			headers: {
 				'x-auth-token': localStorage.getItem('auth')
 			}
@@ -91,16 +105,29 @@ export default class Orders extends Component {
 						<Table.Row>
 							<Table.HeaderCell colSpan='15'>
 								<Menu floated='right' pagination>
-									<Menu.Item as='a' icon>
+									{/* <Menu.Item as='a' icon>
 										<Icon name='chevron left' />
-									</Menu.Item>
-									<Menu.Item as='a'>1</Menu.Item>
-									<Menu.Item as='a'>2</Menu.Item>
-									<Menu.Item as='a'>3</Menu.Item>
+									</Menu.Item> */}
+									{
+										(this.state.pageNumber > 1)?
+										<Menu.Item as='a' onClick={()=>{
+											this.setState({pageNumber: this.state.pageNumber - 1}, ()=>{
+												this.handleUpdatePage();
+											})
+										}}>Previous</Menu.Item>
+										:
+										null
+									}
+									<Menu.Item as='a' onClick={()=>{
+										this.setState({pageNumber: this.state.pageNumber + 1}, ()=>{
+											this.handleUpdatePage();
+										})
+									}}>Next</Menu.Item>
+									{/* <Menu.Item as='a'>3</Menu.Item>
 									<Menu.Item as='a'>4</Menu.Item>
 									<Menu.Item as='a' icon>
 										<Icon name='chevron right' />
-									</Menu.Item>
+									</Menu.Item> */}
 								</Menu>
 							</Table.HeaderCell>
 						</Table.Row>
