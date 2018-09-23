3b877f6380817cc0bda483c49d50865c5c026790
omarwaleed <omarpersonalprofile@gmail.com>
2018-09-23 11:48:51 +0200

    fixed order of config params

--- routes/helpers/config.js
+++ routes/helpers/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	jwtSecret: process.env.JWT_SECRET || 'sUG6akIrGG4T5eEGTc6Caau825btqttL1uPZlCqx',
 	paymob: {
-		username: "MadeInEgypt" || process.env.PAYMOB_USERNAME,
-		password: "MyP@ssw0rd1" || process.env.PAYMOB_PASSWORD,
+		username: process.env.PAYMOB_USERNAME || "MadeInEgypt",
+		password: process.env.PAYMOB_PASSWORD || "MyP@ssw0rd1",
 		cardIntegrationId: "1141" || process.env.PAYMOB_CID
 	},
 	shippingFees: parseInt(process.env.SHIPPING_FEE) || 20
