7b2bc4c481e8152c326b30e74cc45449e2c2a9a7
omarwaleed <omarpersonalprofile@gmail.com>
2018-10-08 22:18:16 +0200

    logging conditions

--- routes/api.js
+++ routes/api.js
@@ -1249,7 +1249,7 @@ router.route('/products')
 		let shouldSendFCM = false;
 		theProduct.details.forEach((detail)=>{
 			let newDetail = details.find((oneDetail)=>oneDetail.size === detail.size);
-			console.log("OLD detail", detail, "New detail", newDetail);
+			console.log("OLD detail", detail, "New detail", newDetail, !!newDetail, !!(detail.quantity < 6), !!(newDetail >= 6));
 			if(newDetail && detail.quantity < 6 && newDetail >= 6){
 				shouldSendFCM = true;
 			}
